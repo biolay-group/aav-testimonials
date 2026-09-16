b5821ebae8db46e4e0b7c84b07297cc6505cbfcf
Jean-Baptiste Biolay <jb@biolaygroup.com>
2026-09-16 09:51:38 +0200

    Update author name in aav-testimonials.php

--- aav-testimonials.php
+++ aav-testimonials.php
@@ -5,7 +5,7 @@
  * Version:           1.5.4
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Author:            Jean-Baptiste Biolay
+ * Author:            Biolay Group
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       aav
